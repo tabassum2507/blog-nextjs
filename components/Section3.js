77cdf453d8455a4983bf37758fac694bd175e735
tabassum2507 <10tabassum2@gmail.com>
2023-03-13 18:13:11 +0530

    section3

--- components/Section3.js
+++ components/Section3.js
@@ -1,7 +1,38 @@
+import Link from "next/link"
+import Image from "next/image"
+import Author from "./_child/author"
 
 const Section3 = () => {
   return (
-    <div>Section3</div>
+    <section className="container mx-auto md:px-20 py-16">
+    <h1 className="font-bold text-4xl py-12 text-center">Most Popular</h1>
+
+    <Post />
+    </section>
+
+
+  )
+}
+
+function Post() {
+  return(
+    <div className="grid">
+            <div className="images">
+                <Link href={"/"}><Image src={"/images/img1.jpg"} width={600} height={400} /></Link>
+            </div>
+            <div className="info flex justify-center flex-col py-4">
+                <div className="cat">
+                    <Link href={"/"}className="text-orange-600 hover:text-orange-800">category</Link>
+                    <Link href={"/"}className="text-gray-800 hover:text-gray-600">- published</Link>
+                </div>
+                <div className="title">
+                    <Link href={"/"} className="text-3xl md:text-4xl font-bold text-gray-800 hover:text-gray-600">title</Link>
+                </div>
+                <p className="text-gray-500 py-3">
+                </p>
+                <Author />
+            </div>
+        </div>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import styles from './page.module.css'
 import Format from "../../layout/Format.js"
 import Section1 from '../../components/Section1'
 import Section2 from '../../components/Section2'
+import Section3 from '../../components/Section3'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,6 +15,7 @@ export default function Home() {
       <Format >
         <Section1 />
         <Section2 />
+        <Section3 />
       </Format>
     </div>
   )
