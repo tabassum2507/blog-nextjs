5d6453cd0b2835e99c69da0a5766e26c91d00525
tabassum2507 <10tabassum2@gmail.com>
2023-03-13 16:54:54 +0530

    section1

--- components/Section1.js
+++ components/Section1.js
@@ -1,55 +1,49 @@
-
+import Link from "next/link"
 const Section1 = () => {
+  
+    const bg = {
+        background: "url('/images/banner.png') no-repeat",
+        backgroundPosition: "right"
+    }
+
   return (
-    <section className="py-16" style={bg}>
+    <section className="py-16" style={bg} >
         <div className="container mx-auto md:px-20">
             <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
 
-            <Swiper
-                slidesPerView={1}
-                // loop={true}
-                // autoplay= {{
-                //     delay: 2000
-                // }}
-                >
-                {
-                    data.map((value, index) => (
-                        <SwiperSlide key={index}><Slide data={value}></Slide></SwiperSlide>
-                    ))
-                }
-            ...
-            </Swiper>
-
+        <Slide />
             
         </div>
     </section>
   )
 }
 
-function Slide({ data }){
-
-    const { id, title, category, img, published, description ,author } = data;
-
-    return (
+function Slide(){
+    return(
         <div className="grid md:grid-cols-2">
             <div className="image">
-                <Link href={`/posts/${id}`}><a><Image src={img || "/"} width={600} height={600} /></a></Link>
+                <Link href={"/"}>
+                <img src={"/images/img1.jpg"} width={600} height={600} />
+                </Link>
             </div>
+
             <div className="info flex justify-center flex-col">
-                <div className="cat">
-                    <Link href={`/posts/${id}`}><a className="text-orange-600 hover:text-orange-800">{category || "Unknown"}</a></Link>
-                    <Link href={`/posts/${id}`}><a className="text-gray-800 hover:text-gray-600">- {published || "Unknown"}</a></Link>
+               <div className="cat">
+                <Link href={"/"} className="text-orange-600 hover:text-orange-800">Business, Travel</Link>
+                <Link href={"/"} className="text-gray-800 hover:text-gray-600"> - July, 2022</Link>
+               </div>
+
+               <div className="title">
+                    <Link href={"/"} className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">title</Link>
                 </div>
-                <div className="title">
-                    <Link href={`/posts/${id}`}><a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">{title || "Unknown"}</a></Link>
-                </div>
+
                 <p className="text-gray-500 py-3">
-                    {description || "description"}
+                    description
                 </p>
-                { author ? <Author {...author}></Author> : <></>}
+                <h1>Author</h1>
             </div>
         </div>
-  )
+    )
 }
 
 export default Section1
