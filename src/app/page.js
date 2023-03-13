b42f25b200ed4749b613a143c27c10211e255640
tabassum2507 <10tabassum2@gmail.com>
2023-03-13 18:38:20 +0530

    section4

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Format from "../../layout/Format.js"
 import Section1 from '../../components/Section1'
 import Section2 from '../../components/Section2'
 import Section3 from '../../components/Section3'
+import Section4 from '../../components/Section4'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +17,7 @@ export default function Home() {
         <Section1 />
         <Section2 />
         <Section3 />
+        <Section4 />
       </Format>
     </div>
   )
